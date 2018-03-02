7a14c1e00ed6ad6142e028d2fa583be0aa8747f6
Zach Breit <zbreit18@gmail.com>
2018-03-02 14:12:57 -0500

    Fixed the elevator and other current limiting code

--- 2018 Robot/src/Constants.h
+++ 2018 Robot/src/Constants.h
@@ -22,6 +22,8 @@ namespace consts
 	constexpr double ELEVATOR_SETPOINTS[NUM_ELEVATOR_SETPOINTS] = {3, 20, 40, 60, 70};
 	constexpr double ELEVATOR_INCREMENT_PER_CYCLE = 35. / 20.; // 35" per second
 	constexpr double ELEVATOR_SPEED_REDUCTION = 1 / 3;
+	constexpr int ELEVATOR_CONT_CURRENT_MAX = 60;
+	constexpr int ELEVATOR_CONT_CURRENT_TIMEOUT_MS = 2000;
 
 	constexpr double MIN_DISTANCE_TO_CUBE = 9.0;
 	constexpr double INTAKE_SPEED = 0.7;
